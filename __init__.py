3513760284779e6253fc15dcf4afafd63459dd06
Chenyun Z <zhang.chenyun@outlook.com>
2024-11-17 11:55:15 +0800

    Auth GOGOGO!

---
+++ __init__.py
@@ -0,0 +1,11 @@
+import os
+
+# 获取脚本所在的绝对路径
+script_path = os.path.abspath(__file__)
+
+# 获取脚本所在的目录
+script_dir = os.path.dirname(script_path)
+
+with open('path.txt', 'w') as pathes:
+    pathes.write(script_dir)
+    print(f'This script is running at {script_dir}. DON\'T MOVE IT!')
